3398f011cb47216b24a4ed6adff60c43ad970322
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 21:07:11 +0000

    Automatically backed up by Learn

--- db/migrate/2019_create_birds.rb
+++ db/migrate/2019_create_birds.rb
@@ -1,10 +1,9 @@
-# Add code from Readme
 class CreateBirds < ActiveRecord::Migration[5.2]
   def change
     create_table :birds do |t|
       t.string :name
       t.string :species
-
+ 
       t.timestamps
     end
   end
